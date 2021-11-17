0809c998355a65bedc6ed9b2e0c9b5d902539118
rikeshg <89558538+rikeshg@users.noreply.github.com>
2021-11-17 17:29:35 -0600

    committed changes

--- app.js
+++ app.js
@@ -3,12 +3,52 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Costume = require("./models/costume");
+
+const connectionString =  'mongodb+srv://Rikesh:g1213107239@cluster0.6y8j2.mongodb.net/Cluster0?retryWrites=true&w=majority'  
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}); 
+//Get the default connection 
+var db = mongoose.connection; 
+ 
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+ console.log("Connection to DB succeeded")});
+ 
+ 
+// We can seed the collection if needed on server start 
+async function recreateDB(){ 
+  // Delete everything 
+  await Costume.deleteMany(); 
+ 
+  let instance1 = new Costume({costume_type:"ghost",  size:'large', cost:25.4}); 
+  let instance2 = Costume({costume_type:"god",  size:'large', cost:95.8}); 
+  let instance3 = Costume({costume_type:"superheroes",  size:'large', cost:10.8}); 
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  });
+  instance2.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("Second object saved") 
+});
+instance3.save( function(err,doc) { 
+  if(err) return console.error(err); 
+  console.log("Third object saved") 
+}); 
+} 
+ 
+let reseed = true; 
+if (reseed) { recreateDB();} 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var soapsRouter = require('./routes/soaps');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+var resourceRouter = require('./routes/resource');
+
 
 var app = express();
 
@@ -26,6 +66,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/soaps', soapsRouter);
 app.use('/selector', selectorRouter);
+app.use('/resource', resourceRouter);
+app.use('/costumes', resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
